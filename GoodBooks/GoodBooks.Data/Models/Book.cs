20258237cfec2ab4a69e478349e61cd58779f721
ricardoaguiar <barrios.dev@gmail.com>
2021-06-29 15:49:37 +0200

    p2 added book classes

---
+++ GoodBooks/GoodBooks.Data/Models/Book.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace GoodBooks.Data.Models
+{
+    public class Book
+    {
+    public int Id{ get; set; }
+    public DateTime CreatedOn{ get; set; }
+    public DateTime UpdatedOn{ get; set; }
+    public string Title{ get; set; }
+    public string Author{ get; set; }
+
+  }
+}
---
+++ GoodBooks/GoodBooks.Data/Models/BookReview.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace GoodBooks.Data.Models
+{
+    public class BookReview
+    {
+    public int Id{ get; set; }
+    public DateTime CreatedOn{ get; set; }
+    public DateTime UpdatedOn{ get; set; }
+    public string ReviewAuthor{ get; set; }
+    public string ReviewContent{ get; set; }
+    }
+}
